457ec8eda7d75ce6704eafb9563a7bc70b395ac9
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-08-23 15:19:39 +0530

    Update Category.java

--- EcommerceBackend/src/main/java/com/tyagi/EcommerceBackend/model/Category.java
+++ EcommerceBackend/src/main/java/com/tyagi/EcommerceBackend/model/Category.java
@@ -13,21 +13,27 @@ public class Category
 @GeneratedValue
 int catid;
 String catname,catdesc;
+	
 public int getCatid() {
 	return catid;
 }
+	
 public void setCatid(int catid) {
 	this.catid = catid;
 }
+	
 public String getCatname() {
 	return catname;
 }
+	
 public void setCatname(String catname) {
 	this.catname = catname;
 }
+	
 public String getCatdesc() {
 	return catdesc;
 }
+	
 public void setCatdesc(String catdesc) {
 	this.catdesc = catdesc;
 }
