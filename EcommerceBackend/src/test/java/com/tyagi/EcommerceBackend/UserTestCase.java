ff69bb478ce4055a7bc02ec1ed1bcb5594b60205
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-08-23 15:40:49 +0530

    Update UserTestCase.java

--- EcommerceBackend/src/test/java/com/tyagi/EcommerceBackend/UserTestCase.java
+++ EcommerceBackend/src/test/java/com/tyagi/EcommerceBackend/UserTestCase.java
@@ -14,24 +14,17 @@ public static void main(String[] args) {
 	context.refresh();
 
 UserDAO userDAO=(UserDAO)context.getBean("userDAO");
-
 	
 UserRegister user=new UserRegister();
-
 user.setAddress("Hapur");
 user.setPassword("Tyagi123");
 user.setCustomername("Deepak");	
-
-
 user.setEmail("19aakash33@gmail.com");
 user.setMobile(971121);
-
 userDAO.insertUpdateUser(user);		
 System.out.println("User Inserted");
 
-
 //Retrieval TestCase
-
 		/*UserRegister user1=userDAO.getUser("");
 		System.out.println("User Name:"+user1.getUsername());
 		System.out.println("User Description:"+user1.getRole());
@@ -42,9 +35,7 @@ System.out.println("User Inserted");
 		System.out.println("The User Deleted");
 		
 		//Retrieving the Data
-		
 		List<UserRegister> list=userDAO.getUserDetails();
-		
 		for(UserRegister user3:list)
 		{
 			System.out.println(user3.getUsername()+":"+user3.getPassword()+":"+user3.getRole());
@@ -54,9 +45,6 @@ System.out.println("User Inserted");
 		UserRegister user4=userDAO.getUser("");
 		user4.setRole("");
 		userDAO.insertUpdateUser(user4);
-		System.out.println("The User Updated");*/
-		
-	
+		System.out.println("The User Updated");*/	
+      }
 }
-}
-
